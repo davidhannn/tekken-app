a82ae5cef2bb2e07d681f1743bd96064e3620690
David Han <email>
2020-11-09 18:45:49 -0800

    Updates

---
+++ precache-manifest.7cfa613e534a6842ee6e6afcb89694cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3de0cb6f08fa1a32cf30112e854a4e9",
+    "url": "/davidhannn/tekken-app/index.html"
+  },
+  {
+    "revision": "2e2ad482c1a8a29f2edd",
+    "url": "/davidhannn/tekken-app/static/css/main.c3e69b1b.chunk.css"
+  },
+  {
+    "revision": "31f25953889b0ac4cfff",
+    "url": "/davidhannn/tekken-app/static/js/2.54822618.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/davidhannn/tekken-app/static/js/2.54822618.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e2ad482c1a8a29f2edd",
+    "url": "/davidhannn/tekken-app/static/js/main.8eff5aab.chunk.js"
+  },
+  {
+    "revision": "341ef1095705d8ed2275",
+    "url": "/davidhannn/tekken-app/static/js/runtime-main.4e7a4692.js"
+  }
+]);
